ea21210fa5358c4c49aaf8b1ee53d121c7a5b3cd
Esther Kang <estherkang.j@gmail.com>
2025-07-21 17:11:31 -0700

    Initial Commit

---
+++ RecipeHub/RecipeHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeHubApp.swift
+//  RecipeHub
+//
+//  Created by Esther Kang on 7/21/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipeHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
